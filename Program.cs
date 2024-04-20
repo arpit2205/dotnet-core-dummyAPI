2847e50f0bdb14c85a225efb31df6e807d99b1ca
Arpit Gupta <arpit.8.a@gmail.com>
2024-04-20 20:40:09 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using NSwag.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument(config =>
+{
+    config.DocumentName = "DummyAPI";
+    config.Title = "DummyAPI v1";
+    config.Version = "v1";
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi(config =>
+    {
+        config.DocumentTitle = "DummyAPI";
+        config.Path = "/swagger";
+        config.DocumentPath = "/swagger/{documentName}/swagger.json";
+        config.DocExpansion = "list";
+    });
+}
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
